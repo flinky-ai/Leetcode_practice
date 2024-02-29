edb08dae776d05fda92e6cd5004ebe8c3bbe771a
Renguang <493282613@qq.com>
2024-02-29 17:26:22 +0800

    Update 2799.py

--- 2799.py
+++ 2799.py
@@ -1,23 +1,25 @@
-nums = [1, 3, 1, 2, 2]
+nums = [1, 3, 1, 1, 2]
 
 tar = len(set(nums))
 N = len(nums)
 count = 0
 
 for i in range(0, N - tar + 1):
-    print(i)
+    print(i, N - tar + 1)
     aux = set(nums[i:i+tar])
     print(aux)
     test = True
     print(test)
     for j in range(tar + i, N):
-        print(tar + i,N)
+        print(j, tar + i, N, len(aux))
         if len(aux) < tar:
             aux.add(nums[j])
+            print(aux)
         else:
             v = N - j + 1
             count += v
             test = False
+            print(count)
             break
     if test and len(aux) >= tar:
             count += 1
