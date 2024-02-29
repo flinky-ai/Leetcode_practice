6ab033157f39e6eb7ec45efd56e72094803eee24
Renguang <493282613@qq.com>
2024-02-29 18:48:10 +0800

    Create 1609.py

---
+++ 1609.py
@@ -0,0 +1,27 @@
+root = [5,4,2,3,3,7]
+
+class node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+if root is None:
+    print(True)
+queue = [root]
+level = 0
+while queue:
+    prev = float('-inf') if level % 2 == 0 else float('inf')
+    for _ in range(len(queue)):
+        node = queue.pop(0)
+        if (level % 2 ==0 and (node.val % 2 == 0 or node.val <= prev)) or (level % 2 != 0 and (node.val % 2 != 0 or node.val >= prev)):
+            print(False)
+
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+        prev = node.val
+    level +=1
+print(True)
